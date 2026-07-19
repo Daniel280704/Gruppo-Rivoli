f36bde5143d76b78bdb53dee32963d97cf5f3484
Daniel <154078145+Daniel280704@users.noreply.github.com>
2026-07-19 21:58:53 +0200

    Update report_climatico.py

--- report_climatico.py
+++ report_climatico.py
@@ -312,11 +312,6 @@ def main():
 
 
     
-
-    
-    process_period('month', 2026, target_month=3)
-    print("In attesa 30 secondi..."); time.sleep(30)
-    
     process_period('month', 2026, target_month=4)
     print("In attesa 30 secondi..."); time.sleep(30)
     
